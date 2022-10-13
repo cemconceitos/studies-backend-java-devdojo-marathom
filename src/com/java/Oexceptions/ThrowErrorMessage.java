00c645140f727ab496a3a60d912191509d980bac
semluiz <lukalds@gmail.com>
2022-10-13 20:08:50 -0300

    material de suporte curso java

---
+++ src/com/java/Oexceptions/ThrowErrorMessage.java
@@ -0,0 +1,44 @@
+package com.java.Oexceptions;
+
+import java.io.IOException;
+
+/***
+ *  99 - CLASS JAVA CONCEPTS
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * **/
+
+
+public class ThrowErrorMessage {
+
+    public static void main(String[] args) {
+        try {
+            int idade = 0;
+
+            private void validatingUserAge() throws IOException {
+                if (idade < 18) {
+
+                }
+            };
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+/****
+ *
+ *  - DOUBTS: ioException, runrtime excecption
+ *  - CONCEPT: throw is mainly used to throw personalized exceptions
+ *
+ *
+ *
+ *
+ *
+ * **/
